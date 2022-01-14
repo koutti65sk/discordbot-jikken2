ac079242c0bf9301343ac9e7d0d2768b206f2aeb
飯塚晃生 <kou605088@icloud.com>
2022-01-14 16:16:55 +0900

    main

---
+++ discordbot.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+from os import getenv
+import traceback
+
+bot = commands.Bot(command_prefix='/')
+
+errorchannel = 931445563654815776
+logchannel = 928951765867585536
+dmchannel = 931445891481624626
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    channel = bot.get_channel(errorchannel)
+    await channel.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(token)
