2100cd8e827f24d6fbf19e883089e0b6fffcb3f1
飯塚晃生 <kou605088@icloud.com>
2022-01-15 18:30:03 +0900

    あ

--- discordbot.py
+++ discordbot.py
@@ -33,13 +33,14 @@ async def on_message_dm(message):
         return
     elif type(message.channel) == discord.DMChannel and bot.user == message.channel.me:
         channels = bot.get_channel(dmchannel)
+        image_url = message.attachments
         embed = discord.Embed(title = "DMを受け取りました。",color = 0x4682B4,url = message.jump_url
         )
         embed.set_author(name = bot.user,icon_url = bot.user.avatar_url
         )
         embed.add_field(name="匿名すこん部",value = message.content)
-        embed.set_image(url = message.attachment)
-        await channel.send(embed = embed)
+        embed.set_image(url = image_url[0])
+        await channels.send(embed = embed)
         return
     else:
         return
