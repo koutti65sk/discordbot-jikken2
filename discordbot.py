4140c63fa37de0f75f994d9951ec08cda980bbd6
koutti65sk <95573358+koutti65sk@users.noreply.github.com>
2022-02-19 22:04:37 +0900

    削除ログ追加

--- discordbot.py
+++ discordbot.py
@@ -1,15 +1,18 @@
 import discord
 from discord.ext import commands
 import os
-import traceback
+import datetime
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/',intents = intents)
+bot = commands.Bot(command_prefix='//',intents = intents)
 
-errorchannel = 931445563654815776
-logchannel = 928951765867585536
-dmchannel = 931445891481624626
+logchannel = 944578561413619712
+delchannel = 944579014054514748
 
+def jst():
+    now = datetime.datetime.utcnow()
+    now = now + datetime.timedelta(hours=9)
+    return now
 
 @bot.event
 async def on_ready():
@@ -17,38 +20,18 @@ async def on_ready():
     await channel.send('login')
     return
 
+@bot.event
+async def on_message_delete(message):
+    now = jst()
+    embed = discord.Embed(title="メッセージ削除", color=discord.Color.red())
+    embed.add_field(name="メッセージ", value=message.content, inline=False)
+    embed.add_field(name="時刻", value=now.strftime('%Y /%m / %d　 %H : %M : %S'), inline=False)
+    embed.add_field(name="チャンネル", value=message.channel.mention, inline=False)
+    embed.set_footer(icon_url=message.author.avatar_url, text=message.author.display_name)
+    channel = bot.get_channel(delchannel)
+    await channel.send(embed=embed)
+    return
 
-async def set_embed(message):
-    embed = discord.Embed(
-        title = "DMを受け取りました。",color = 0x4682B4,url = message.jump_url,description = message.content
-        )
-    embed.set_author(
-    name = bot.user,icon_url = bot.user.avatar_url
-    )
-    return embed
-
-@bot.listen('on_message')
-async def on_message_dm(message):
-    if message.author.bot:
-        return
-    elif type(message.channel) == discord.DMChannel and bot.user == message.channel.me:
-        channel = bot.get_channel(dmchannel)
-        sent_message = []
-        embed = await set_embed(message)
-        sent_message.append(embed)
-        if message.attachments and message.attachments[0].proxy_url:
-            for attachment in message.attachments[0:]:
-                embed = discord.Embed()
-                embed.set_image(
-                url=attachment.proxy_url
-                )
-                sent_message.append(embed)
-        else:
-            return
-        await channel.send(embeds = sent_message)
-        return
-    else:
-        return
 
 token = os.environ["DISCORD_BOT_TOKEN"]
 bot.run(token)
