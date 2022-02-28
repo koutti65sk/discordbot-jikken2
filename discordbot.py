c47c5a38a438a13583a20c2c0dca90c02593541a
koutti65sk <95573358+koutti65sk@users.noreply.github.com>
2022-03-01 00:02:08 +0900

    スイカ割りを実験で追加

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import datetime
+import math
+import random
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='//',intents = intents)
@@ -41,6 +43,50 @@ async def on_message_delete(message):
     channel = bot.get_channel(delchannel)
     await channel.send(embed=embed)
     return
+
+#2点間の距離を求める関数
+def calc_distance(pos1, pos2):
+    diff_x = pos1[0] - pos2[0]
+    diff_y = pos1[1] - pos2[1]
+    
+    return math.sqrt(diff_x**2 + diff_y**2)
+
+#位置決めの関数
+def generate_position(size):
+    x = random.randrange(0, size)
+    y = random.randrange(0, size)
+    return (x, y)
+
+#プレイヤー移動の関数
+def move_position(direction, pos):
+    current_x, current_y = pos
+    if direction == "n":
+        current_y = current_y - 1
+    elif direction == "s":
+        current_y = current_y + 1
+    elif direction == "w":
+        current_x = current_x - 1
+    elif direction == "e":
+        current_x = current_x + 1
+    return (current_x, current_y)
+
+@bot.command()
+async def suika_wari(ctx):
+    BOARD_SIZE = 5  # ボードの初期サイズ
+    suika_pos = generate_position(BOARD_SIZE)
+    player_pos = generate_position(BOARD_SIZE)
+
+    # スイカとプレイヤーの位置が異なる間、処理を繰り返す
+    while (suika_pos != player_pos):
+        # スイカとプレイヤーの距離を表示する
+        distance = calc_distance(suika_pos, player_pos)
+        print("スイカへの距離:", distance)
+
+        # キー入力に応じて、プレイヤーを移動する
+        c = await(input("n:北に移動 s:南に移動 e:東に移動 w:西に移動"))
+        player_pos = move_position(c, player_pos)
+    await ctx.send('スイカを割りました！')
+
 
 
 token = os.environ["DISCORD_BOT_TOKEN"]
